5d12de51ce8d01cd25c2d3797815d63533ab47af
ibrahim4529 <ibrahim.hanif4529@gmail.com>
2022-04-30 20:50:06 +0700

    add pyside2

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print("Versi 0.1")
+import sys
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QApplication, QLabel, QMainWindow
+                                                     
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.resize(400, 400)
+    label = QLabel("Hello World", alignment=Qt.AlignCenter)
+    window.setCentralWidget(label)
+    window.show()
+    sys.exit(app.exec_())
